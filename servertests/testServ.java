a3d714fa40ee0fdfd15b5d08ec1fc82a2d1d8554
mat <bachmem195@potsdam.edu>
2015-03-20 10:16:46 -0400

    Updating the moveServer

--- servertests/testServ.java
+++ servertests/testServ.java
@@ -8,66 +8,127 @@ class testServ extends Thread {
     
 
     //number of clients
-    public static int nclient = 0;
+    private static int nclient = 0;
     //Server port
-    public static final int defaultPort = 4099;
+    private static int defaultPort;
     //server will use this socket
-    public Socket s = null;
+    private Socket s = null;
+    private static String serverName;
+    private Scanner parser;
     
 
     public testServ(Socket s) {
-	this.s = s;
+    	this.s = s;
     }
 
 
     //port number as input
     //opens the server and multi threads the server
-        public static void startServ(int serverPort) throws Exception {
-	ServerSocket svr = new ServerSocket(serverPort);
-	while(true) {
-	    System.out.println("Starting new server");
-	    Socket s = svr.accept();
-	    new testServ(s).start();
-	}
+    public static void startServ(int serverPort) throws Exception {
+    	ServerSocket svr = new ServerSocket(serverPort);
+    	while(true) {
+    		System.out.println("Starting new server");
+    		Socket s = svr.accept();
+    		new testServ(s).start();
+    	}
 
     }
 
 
-    //imput: client socket
-    //runnin stuff
+    //input: client socket
+    //Will run the main game loop and message passing
 
     public void run() {
-	nclient++;
-	System.out.println("Starting thread #" + nclient);
+    	nclient++;
+    	System.out.println("Starting thread #" + nclient);
+    	boolean victor = false;
+    	
+	
 	try {
+		
+	    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	    PrintStream out = new PrintStream(s.getOutputStream());
 	    
+	    //Sends the servers AI identifier
+	    out.println("MOVE-SERVER "+ serverName);
 	    
-	    	    
-	    Scanner in = new Scanner(s.getInputStream());
-	    PrintStream out = new PrintStream(s.getOutputStream()); 
+	    //Recieves the players to signal the start of the game
+	    //will eventually implement a timeout, needs to test for illegal strings
+	    String playerList = in.readLine();
 	    
+	    //Game loop
+		do {
+			System.out.println("In the DO");
+			parser = new Scanner(in.readLine());
+			String message = parser.next();
+			//System.out.println(message);
+			switch (message) {
+				case "GO?": 
+					System.out.println("GO?");
+					/*Place the GO method here
+					 * this method will calculate a move and send back
+					 * out.println("GO" + <move-string>);
+					*/
+					break;
+				case "WENT": 
+					System.out.println("WENT");
+					while (parser.hasNext()) {
+						System.out.println(parser.next());
+					}
 
-	    System.out.println("Server says hi");
-	    int x = in.nextLine();
-	    System.out.println("You said " + x);
-	    out.print("Message sent back");
-	    s.close();
+					/*WENT method here
+					 * INPUT: WENT <player-id> <move-string>
+					 * Will be recieved each turn by every player
+					 * will update the board based upon a players move
+					 */
+					break;
+				case "BOOT": 
+					System.out.println("BOOT");
+					/* BOOT method here
+					 * INPUT BOOT <player-id>
+					 * removes the booted players pawns from the game board
+					 * if this instance is the booted player, break out, s.close();
+					 */
+					break;
+				case "VICTOR": 
+					System.out.println("VICTOR");
+					victor = true;
+					/*VICTOR method
+					 * input: VICTOR <player-id>
+					 * the game is over and <player> has won
+					 */
+					break;
+					
+				default:
+					System.out.println("Unknown Case");
+					//s.close();
+					break;
+			}
 
 
-	    
-	    
+			
+		} while (!victor);
+		
+    	s.close();
+
 	}catch (Exception e) {
 	    System.err.println(e);
 	    System.exit(1);
 	}
-
+    
 
     }
 
     
     
-    public static void main (String [] args) {
-
+    public static void main (String [] args) throws Exception {
+    	
+    	if(args.length > 0) {
+    	defaultPort = Integer.parseInt(args[0]);
+    	serverName = args[1];
+    	} else
+    		throw new Exception("Incorrect syntax. Requires port number, then player id");
+    	
 	try {
 	    startServ(defaultPort);
 	} catch (Exception e) {
